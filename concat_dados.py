b65a824256fcf076ad37450337ce769b52a5e639
dropedesenv <costadrope@gmail.com>
2024-07-16 22:29:11 -0300

    Atualizando desafio

---
+++ concat_dados.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+#VAMOS RECEBER DOI DADOS DIFERENTES DO USUARIO DE CONCATENA-LOS
+
+info1 = input("Digite a primeira informação: ")
+info2 = input("Digite a segunda informação: ")
+
+
+info_concatenada = info1 + " " + info2
+
+print("Informações concatenadas: ", info_concatenada)
---
+++ repeat_txt.py
@@ -0,0 +1,7 @@
+#Vamos vamos solicitar uma string e um numero inteiro como entrada e vamos repetir a string o numero de vezes que foi solicitado.
+
+
+string = input("Digite o texto: ")
+numero = int(input("Digite o número de repetições: "))
+
+print(string * numero)
